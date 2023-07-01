f385810398c6c88ad9a7998d970c5c22266458d5
冯伟 <lyman.von9506@gmail.com>
2023-07-01 22:06:28 +0800

    feat: Setting global fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte}"],
     theme: {
-        extend: {},
-        screens: {
-            sm: "640px",
-        },
-        fontFamily: {
-            mono: ["IBM Plex Mono", "monospace"],
+        extend: {
+            fontFamily: {
+                sans: ["LXGW WenKai Screen", ...defaultTheme.fontFamily.sans],
+                mono: ["IBM Plex Mono", ...defaultTheme.fontFamily.mono],
+            },
         },
     },
     plugins: [require("@tailwindcss/typography"), require("daisyui")],
