ff949301f7c997cd79eb9b4446cec5311e83a16b
mekhi <lyman.von9506@gmail.com>
2023-07-20 11:06:44 +0800

    fix: Update github action

--- .github/actions-scripts/update-remote-submodule.mjs
+++ .github/actions-scripts/update-remote-submodule.mjs
@@ -4,18 +4,16 @@ const octokit = new Octokit({
     auth: process.env.TOKEN,
 });
 
-// await octokit
-//     .request("GET /repos/{owner}/{repo}/issues", {
-//         owner: "github",
-//         repo: "docs",
-//         per_page: 2,
-//     })
-//     .catch((error) => {
-//         if (error.status === 404) {
-//             console.log("The repository is not starred by me");
-//         } else {
-//             console.error(
-//                 `An error occurred while checking if the repository is starred: ${error?.response?.data?.message}`,
-//             );
-//         }
-//     });
+// const updateBlogSubmodule = async () => {
+//     const { data: repo } = await octokit.request("GET /repos/{owner}/{repo}");
+// };
+
+await octokit.request("GET /repos/{owner}/{repo}/contents/src/content/blog?ref=blog").catch((error) => {
+    if (error.status === 404) {
+        console.log("The repository is not starred by me");
+    } else {
+        console.error(
+            `An error occurred while checking if the repository is starred: ${error?.response?.data?.message}`,
+        );
+    }
+});
